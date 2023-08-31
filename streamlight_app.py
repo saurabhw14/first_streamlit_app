0fbb27b533c0694c992248a2ab7c7a7c83262b9f
saurabhw14 <140445259+saurabhw14@users.noreply.github.com>
2023-08-31 11:44:15 +0530

    Update streamlight_app.py
    s

--- streamlight_app.py
+++ streamlight_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title("Test")
+streamlit.title("Breakfast Favraoites")
 
 streamlit.header(' 🥗  Breakfast Menu')
 streamlit.text(' 🥗  Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
